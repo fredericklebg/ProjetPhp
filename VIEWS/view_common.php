4f4914a3469df097c6d8cfb56c0de5a57772febc
VincentSD <vincentsd@yahoo.com>
2019-10-18 00:42:59 +0200

    test mvc

--- VIEWS/view_common.php
+++ VIEWS/view_common.php
@@ -64,7 +64,7 @@
 
             ?>
 
-                <form class="registerMenu" action="https://tpphp.alwaysdata.net/ProjetPhp?page=user&action=inscription" method="post">
+                <form class="registerMenu" action="/index.php?page=user&action=inscription" method="post">
                     <input type="text" placeholder="identifiant" name="identifiant" /> <br>
                     <label> sexe </label> <br>
                     <input type="radio" value="homme" name="genre" checked/> homme <br>
