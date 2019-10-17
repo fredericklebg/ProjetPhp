dad5128f3aed9e5ad76a6a682cf2346de8eccc09
VincentSD <vincentsd@yahoo.com>
2019-10-17 22:30:06 +0200

    test mvc

--- VIEWS/view_common.php
+++ VIEWS/view_common.php
@@ -64,7 +64,7 @@
 
             ?>
 
-                <form class="registerMenu" action="/CONTROLLERS/controller_main.php" method="post">
+                <form class="registerMenu"  method="post">
                     <input type="text" placeholder="identifiant" name="identifiant" /> <br>
                     <label> sexe </label> <br>
                     <input type="radio" value="homme" name="genre" checked/> homme <br>
