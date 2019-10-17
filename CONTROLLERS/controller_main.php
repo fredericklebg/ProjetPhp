39fbd7e3bcc581c3d8fff690ff626e51d503efb0
VincentSD <vincentsd@yahoo.com>
2019-10-17 21:49:14 +0200

    test mvc

--- CONTROLLERS/controller_main.php
+++ CONTROLLERS/controller_main.php
@@ -57,7 +57,7 @@ class controller_main
             }
 
             else {  // aucune page définie : affichage de l'accueil
-                $vue = new Vue("profilePage");
+                $vue = new Vue("accueil");
                 $vue->generer(array());
             }
         }
