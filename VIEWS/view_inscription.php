a53d032a45d32210ebd2efa2c521bb26f6b81fd7
VincentSD <vincentsd@yahoo.com>
2019-10-17 22:18:20 +0200

    test mvc

--- VIEWS/view_inscription.php
+++ VIEWS/view_inscription.php
@@ -1,4 +1,5 @@
 <?php $titre = 'inscription'; ?>
 
+<?php  echo $_POST['identifiant']; ?>
 <p>Votre inscription est terminée !  </p>
 <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
