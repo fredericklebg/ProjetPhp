cafb5a81b7647a9b70cdc87bc8ee1d23ee9a3662
VincentSD <vincentsd@yahoo.com>
2019-10-18 23:35:15 +0200

    mvc++

--- VIEWS/view_inscription.php
+++ VIEWS/view_inscription.php
@@ -2,9 +2,7 @@
 <div class="container">
 
     <section class="col-lg-12">
-        <div class="row bbloc">
             <p>Votre inscription est terminée !  </p>
             <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
-        </div>
     </section>
 </div>
