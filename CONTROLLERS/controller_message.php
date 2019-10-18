541ff5de6993ce8c12a5dff55f02d5687b1a90fb
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-10-18 14:05:23 +0200

    test mvc

---
+++ CONTROLLERS/controller_message.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'MODELS/models_message.php';
+require_once 'VIEWS/view_template.php';
+
+class controller_message
+{
+    private $message;
+
+    public function __construct()
+    {
+        $this->message = new message();
+
+    }
+
+
+
+    }
