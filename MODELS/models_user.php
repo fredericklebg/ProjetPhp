5ba76a2d715361b53c514515edaee97527eac9f0
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-18 18:02:07 +0200

    page changement mdp

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -272,11 +272,14 @@ class user extends base
         $hashedPass = hash('sha256',$password);
 
 
+        $sql = $this->loadDb()->prepare("SELECT * FROM USER WHERE  pseudo= :pseudo AND password= :password");
+        $sql->bindValue(':pseudo',$login,PDO::PARAM_STR);
+        $sql->bindValue(':password',$hashedPass,PDO::PARAM_STR) ;
+        $sql->execute();
 
-
-        //plus rapide*
-        $sql = $this->loadDb()->prepare("SELECT * FROM USER WHERE  pseudo= ? AND password= ?");
-        $sql->execute(array($login, $hashedPass));
+        //plus rapide mais moins sécure
+        //$sql = $this->loadDb()->prepare("SELECT * FROM USER WHERE  pseudo= ? AND password= ?");
+        //$sql->execute(array($login, $hashedPass));
 
 
         if(!preg_match('#^[a-zA-Z0-9_]*$#', $login))
