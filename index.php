cd3c9aa96c7f5123273e4ab8d9896c9182e169ec
VincentSD <vincentsd@yahoo.com>
2019-10-18 21:59:46 +0200

    test de la couille

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+
+
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
 require_once 'CONTROLLERS/router.php';
 require_once 'CONTROLLERS/controller_main.php';
 
