34256371127be946a74f8e905903ca6afefe9f2b
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-10-18 13:59:02 +0200

    test mvc

---
+++ CONTROLLERS/controller_discussion.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'MODELS/models_discussion.php';
+require_once 'VIEWS/view_template.php';
+
+class controller_discussion
+{
+
+    private $discussion;
+
+    public function __construct() {
+        $this->discussion = new discussion();
+    }
+
+    public function createDiscussion()
+    {
+        $this->discussion->createDiscussion();
+    }
+}
