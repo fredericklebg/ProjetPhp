92ff2e95069c2d1237e474b879a6b90a74c2906c
VincentSD <vincentsd@yahoo.com>
2019-10-19 21:59:34 +0200

    footer

--- VIEWS/view_erreur.php
+++ VIEWS/view_erreur.php
@@ -5,7 +5,7 @@
     <section class="col-lg-12">
         <div  class="row">
 
-    <p >Une erreur est survenue : <?= $msgErreur ?></p>
+    <p >Une erreur est survenue : <?= $msgErreur ?></p> <br/>
             <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
         </div>
     </section>
