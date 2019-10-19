0ce822459036d9defbaca4fce0e0f9da12885945
VincentSD <vincentsd@yahoo.com>
2019-10-19 22:03:34 +0200

    footer

--- VIEWS/view_erreur.php
+++ VIEWS/view_erreur.php
@@ -2,7 +2,7 @@
 
 <div class="container">
 
-    <section class="col-lg-12">
+    <section class="col-lg-12 text-center">
 
     <p >Une erreur est survenue : <?= $msgErreur ?> <br> </p>
         <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
--- VIEWS/view_inscription.php
+++ VIEWS/view_inscription.php
@@ -1,7 +1,7 @@
 <?php $titre = 'inscription'; ?>
 <div class="container">
 
-    <section class="col-lg-12">
+    <section class="col-lg-12 text-center">
             <p>Votre inscription est terminée !  </p>
             <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
     </section>
