3c6d79f9d313ac75b3894783752b309404e6d729
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-21 11:55:20 +0200

    mdp forgot

--- VIEWS/view_sendMdp.php
+++ VIEWS/view_sendMdp.php
@@ -1,6 +1,6 @@
 
 
-    <form class="changebloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/VIEWS/view_accueil.php?page=user&action=sendMdp" method="post">
+    <form class="changebloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=sendMdp" method="post">
         <input type="text" placeholder="mail" name="mail" /> <br>
         <input type="submit" name="action" value="envoyer"/> <br>
     </form>
