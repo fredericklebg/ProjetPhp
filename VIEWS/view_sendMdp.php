639672c863a4a5ee6e653b391b586bbda0789b5b
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-21 12:03:16 +0200

    mdp forgot

---
+++ VIEWS/view_sendMdp.php
@@ -0,0 +1,12 @@
+<?php $this->title ='mot de passe oublié'; ?>
+
+<div class="container">
+
+    <section class="col-lg-12 text-center">
+
+        <p >Votre message a bien été envoyé  <br> </p>
+        <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
+
+    </section>
+</div>
+
