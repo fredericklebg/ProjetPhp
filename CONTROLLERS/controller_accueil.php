6c472a62ce189fb012d5db04e844553a21820057
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-21 11:43:46 +0200

    mdp forgot

--- CONTROLLERS/controller_accueil.php
+++ CONTROLLERS/controller_accueil.php
@@ -37,7 +37,7 @@ class controller_accueil extends controller_main
     }
     public function sendMdp()
     {
-        $vue = new Vue ($_GET['page']);
+        $vue = new Vue ($_GET['sendMdp']);
         $vue->generer(array());
         $this->user->sendMdp();
     }
