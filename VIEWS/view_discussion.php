ac67712bcc84473b2c3c2eed8cb7a9399e10f204
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-21 17:32:39 +0200

    gromit and push

--- VIEWS/view_discussion.php
+++ VIEWS/view_discussion.php
@@ -4,7 +4,7 @@
 <form class="discbloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=discussion&action=createDiscussion" method="post">
         <input type="text" placeholder="nom_discussion" name="nomDisc" /> <br>
         <input type="text" placeholder="new_message" name="newMsg" /> <br>
-        <input type="submit" name="action" value="new_discussion"/> <br>
+        <input type="submit" name="action" value="createDiscussion"/> <br>
     </form>
 
 
