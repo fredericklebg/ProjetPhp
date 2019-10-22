5eb3002aa6c0f030aeae8bcc3acf268125605c26
Ziane Anthony <ant>
2019-10-22 17:52:14 +0200

    discussion

--- VIEWS/view_affichdisc.php
+++ VIEWS/view_affichdisc.php
@@ -1,4 +1,6 @@
 <?php $this->title ='Page Discussion'; ?>
 
 
-
+<div class="TitreD">
+    <p> Titre Discussion </p>
+</div>
--- VIEWS/view_newDiscussion.php
+++ VIEWS/view_newDiscussion.php
@@ -4,8 +4,10 @@
 <form class="discbloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=discussion&action=createDiscussion" method="post">
         <input type="text" placeholder="nom discussion" name="nomDisc" /> <br>
         <input type="text" placeholder="nouveau message" name="newMsg" /> <br>
+    <div class="DiscLien">
         <a href="http://tpphp.alwaysdata.net/ProjetPhp/?page=discussion&action=affichdisc">
         <input type="submit" name="action" value="createDiscussion"/> <br>
+    </div>
     </form>
 
 
