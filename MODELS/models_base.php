cca301c462dbbbba0052a4e8183515f408ad4e20
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-23 15:16:57 +0200

    modif texte accueil

--- MODELS/models_base.php
+++ MODELS/models_base.php
@@ -17,7 +17,8 @@ class base
 
     public function get($attribut,$pseudo)
     {
-        $query = $this->loadDb()->prepare('SELECT '.$attribut.' FROM USER WHERE pseudo= :pseudo AND password= :password');
+        $query = $this->loadDb()->prepare('SELECT attribut FROM USER WHERE pseudo= :pseudo AND password= :password');
+        $query->bindValue('attribut',$attribut,PDO::PARAM_STR);
         $query->bindValue(':pseudo',$pseudo,PDO::PARAM_STR);
         $query->bindValue(':password',$_SESSION['password'],PDO::PARAM_STR);
         $query->execute();
