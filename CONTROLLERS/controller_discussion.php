ee1424a278393927294a87d35b8dc93e85300c51
VincentSD <vincentsd@yahoo.com>
2019-10-24 19:16:50 +0200

    prenez un scooter

--- CONTROLLERS/controller_discussion.php
+++ CONTROLLERS/controller_discussion.php
@@ -21,7 +21,7 @@ class controller_discussion extends controller_main
         $id=$this->discussion->createDiscussion();
         //ajouter message avec addMessage
         $this->msg->addMessage($id);
-        $vue = new Vue('affichdisc');
+        $vue = new Vue('accueil');
         $vue->generer(array());
 
     }
