2b2dd4f0cb2f76be4b73b8530c8600855d8fd6af
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-24 20:21:01 +0200

    prenez un scooter

--- MODELS/models_user.php
+++ MODELS/models_user.php
@@ -435,6 +435,7 @@ class user extends base
 //    }
       public function  sendToken() {
           $mailTok = $_POST['mailTok'];
+          $_SESSION['mailTok']=$mailTok;
           $query = $this->loadDb()->prepare('SELECT mail FROM USER WHERE mail = :mail');
           $query->bindValue(':mail',$mailTok,PDO::PARAM_STR);
           $query->execute();
@@ -455,7 +456,7 @@ class user extends base
             throw new Exception('vous êtes déjà connecté');
         }
 
-        $mailTok = $_POST['mailTok'];
+        $mailTok = $_SESSION['mailTok'];
         $token = $_SESSION['token'];
         $newMdp=$_POST['newMdp'];
         $confirmMdp=$_POST['confirmMdp'];
