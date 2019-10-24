b96d9d477c1caed556459e187100a25c17bee143
fredericklebg <vincent.simonetti-diez@etu.univ-amu.fr>
2019-10-24 16:58:35 +0200

    prenez un scooter

--- VIEWS/view_accueil.php
+++ VIEWS/view_accueil.php
@@ -5,7 +5,7 @@
 
 <?php
 
-
+$disc= new discussion();
 
 ?>
 
@@ -36,14 +36,14 @@
 
                 <table class="col-lg-12">
                     <tr>
-                        <td><?php  ?></td>
+                        <td> nom de la discussion </td>
                         <td>Statut</td>
                         <td>Dernier message</td>
                         <td>Auteur</td>
                         <td>Date</td>
                     </tr>
                     <tr>
-                        <td>Nom de la discussion</td>
+                        <td><?php $disc->getTitle(18);  ?></td>
                         <td>Statut</td>
                         <td>Dernier message</td>
                         <td>Auteur</td>
