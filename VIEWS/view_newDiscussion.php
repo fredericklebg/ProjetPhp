568124e1abaad7a860206e21a7ba521a522e7d42
VincentSD <vincentsd@yahoo.com>
2019-10-25 01:00:23 +0200

    pagination

--- VIEWS/view_newDiscussion.php
+++ VIEWS/view_newDiscussion.php
@@ -7,10 +7,7 @@
 <form class="discbloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=discussion&action=createDiscussion" method="post">
         <input type="text" placeholder="nom discussion" name="nomDisc" /> <br>
         <input type="text" placeholder="nouveau message" name="newMsg" /> <br>
-    <div class="DiscLien">
-        <a href="http://tpphp.alwaysdata.net/ProjetPhp/?page=discussion&action=affichdisc">
         <input type="submit" name="action" value="createDiscussion"/> <br>
-    </div>
     </form>
     </section>
 </div>
