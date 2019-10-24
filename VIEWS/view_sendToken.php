c91c9fab822150b50adbb6fffb5b84852fb30484
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-24 18:53:23 +0200

    prenez un scooter

--- VIEWS/view_sendToken.php
+++ VIEWS/view_sendToken.php
@@ -4,9 +4,8 @@
 
     <section class="col-lg-12 text-center">
         <p >Votre code a bien été envoyé  par mail<br> </p>
-        <a href="http://tpphp.alwaysdata.net/ProjetPhp"> retourner a la page d'accueil </a>
         <form class="changebloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=passChanged" method="post">
-            <input type="password" placeholder="ancien mot de passe" name="token" /> <br>
+            <input type="text" placeholder="Entrer le code" name="token" /> <br>
             <input type="password" placeholder="nouveau mot de passe" name="newMdp"/> <br>
             <input type="password" placeholder="confirmer mot de passe" name="confirmMdp"/> <br>
             <input type="submit" name="action" value="changePass" placeholder="modifier"/> <br>
