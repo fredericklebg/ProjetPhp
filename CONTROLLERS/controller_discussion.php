1c518546dc780be111a4993070ef4874f8c35a85
VincentSD <vincentsd@yahoo.com>
2019-10-27 18:00:50 +0100

    pagination

--- CONTROLLERS/controller_discussion.php
+++ CONTROLLERS/controller_discussion.php
@@ -28,6 +28,7 @@ class controller_discussion extends controller_main
         $id=$this->discussion->createDiscussion();
         //ajouter message avec addMessage
         var_dump($id);
+        exit();
         $this->msg->addMessage($id);
         header("Location: http://tpphp.alwaysdata.net/ProjetPhp");
 
