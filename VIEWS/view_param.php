b68a70377ac7d03beed938bf1c7c90d4eb4f1b3c
Ziane Anthony <ant>
2019-10-27 15:08:14 +0100

    page paramètre

---
+++ VIEWS/view_param.php
@@ -0,0 +1,21 @@
+<?php $this->title = 'paramètres'; ?>
+
+<div class="container">
+
+    <section class="col-lg-12 text-center">
+
+
+        <form class="parambloc"   method="post">
+            <input type="text" placeholder="Saisissez le nombre maximal de discussions" name="d"/>
+            <input type="submit" name="action" value="Valider"/>
+        </form>
+        <form class="parambloc2" method="post">
+            <input type="text" placeholder="Saisissez le nombre maximal de messages" name="d"/>
+            <input type="submit" name="action" value="Valider"/>
+        </form>
+    </section>
+</div>
+
+
+
+
