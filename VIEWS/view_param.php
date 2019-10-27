b2614586e1d1ffd14e60852a0fa32c4f4df6e1c5
Ziane Anthony <ant>
2019-10-27 16:24:24 +0100

    page paramètre

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -1,11 +1,11 @@
-<?php $this->title = 'param'; ?>
+<?php //$this->title = 'param'; ?>
 <div class="container">
 
     <section class="col-lg-12 text-center">
         <form class="parambloc" action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=param"  method="post">
             <input type="text" placeholder="Nombre  de discussions" name="d"/>
             <input type="submit" name="action" value="Valider"/>
-            <input type="text" placeholder="Nombre de messages" name="d"/>
+            <input type="text" placeholder="Nombre de messages" name="d2"/>
             <input type="submit" name="action" value="Valider"/>
         </form>
         <div class="back">
