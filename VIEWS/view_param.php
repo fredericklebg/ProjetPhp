2645fadc973ebad90317ef394084f04fa40b1404
Ziane Anthony <ant>
2019-10-27 16:05:21 +0100

    page paramètre

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -2,7 +2,7 @@
 <div class="container">
 
     <section class="col-lg-12 text-center">
-        <form class="parambloc" action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=paramPage"  method="post">
+        <form class="parambloc" action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=param"  method="post">
             <input type="text" placeholder="Nombre  de discussions" name="d"/>
             <input type="submit" name="action" value="Valider"/>
             <input type="text" placeholder="Nombre de messages" name="d"/>
