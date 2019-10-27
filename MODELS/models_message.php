32f2143d35f033d7c7e6515ddd5cd1546003db7f
VincentSD <vincentsd@yahoo.com>
2019-10-27 17:58:46 +0100

    pagination

--- MODELS/models_message.php
+++ MODELS/models_message.php
@@ -120,6 +120,7 @@ class message extends base
                 NOW()   
          )';
         $this->execRequete($query);
+        var_dump($query);
         $this->message_id=$this->execRequete('SELECT MAX(message_id) FROM MESSAGE');
 //
     }
