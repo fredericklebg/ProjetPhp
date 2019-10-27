28641b0c3f66a346a6426f06ff71f215ed9b4db2
Ziane Anthony <ant>
2019-10-27 15:34:10 +0100

    page paramètre

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -1,21 +1,21 @@
 <?php //$this->title = 'paramètres'; ?>
-
 <div class="container">
 
     <section class="col-lg-12 text-center">
-
-
-        <form class="parambloc"   method="post">
-            <input type="text" placeholder="Saisissez le nombre maximal de discussions" name="d"/>
+        <form class="parambloc" action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=paramPage"  method="post">
+            <input type="text" placeholder="Nombre  de discussions" name="d"/>
+            <input type="submit" name="action" value="Valider"/>
+            <input type="text" placeholder="Nombre de messages" name="d"/>
             <input type="submit" name="action" value="Valider"/>
         </form>
-        <form class="parambloc2" method="post">
-            <input type="text" placeholder="Saisissez le nombre maximal de messages" name="d"/>
-            <input type="submit" name="action" value="Valider"/>
-        </form>
+        <div class="back">
+            <a href="http://tpphp.alwaysdata.net/ProjetPhp" >retourner à la page d'accueil</a>
+        </div>
+
+
+
     </section>
 </div>
-
 
 
 
