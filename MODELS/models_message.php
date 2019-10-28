fe650a5db9a45ee2925c282eb9e13e9a5ab49a85
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-28 19:28:05 +0100

    car oui

--- MODELS/models_message.php
+++ MODELS/models_message.php
@@ -199,10 +199,10 @@ class message extends base
 
     public function verifMsg () {
 
-        if(preg_match("#^[ ]*[a-zA-Z0-9']+[ ]*$#",$_POST['msg'])) return true;
-        if(preg_match("#^[ ]*[a-zA-Z0-9]+[-]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9]*$#",$_POST['msg'])) return true;
+        if(preg_match("#^[ ]*[a-zA-Z0-9']+[ ]*[a-zA-Z0-9']+[ ]*$#",$_POST['msg'])) return true;
+        if(preg_match("#^[ ]*[a-zA-Z0-9]+[-]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9]+[ ]*$#",$_POST['msg'])) return true;
         if(preg_match("#^[ ]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9']+[ ]*[a-zA-Z0-9]+[-]*$#",$_POST['msg'])) return true;
-        if(preg_match("#^[ ]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9]*$#",$_POST['msg'])) return true;
+        //if(preg_match("#^[ ]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9]+[ ]*$#",$_POST['msg'])) return true;
         else {
             throw new Exception('Le message est trop grand ou comporte plus de 2 mots');
         }
