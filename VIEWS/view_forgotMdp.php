3d0f9b6b3a846816a879ed571f9328ca573da63a
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-28 12:42:05 +0100

    prenez un scooter

--- VIEWS/view_forgotMdp.php
+++ VIEWS/view_forgotMdp.php
@@ -2,5 +2,5 @@
 
     <form class="changebloc"  action="http://tpphp.alwaysdata.net/ProjetPhp/?page=accueil&action=forgotMdp" method="post">
         <input type="text" placeholder="mail" name="mailTok" /> <br>
-        <input type="submit" name="action" value="sendToken"/> <br>
+        <input type="submit" name="action" value="envoyé !"/> <br>
     </form>
