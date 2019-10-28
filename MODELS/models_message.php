04e1c9735fece0f09eac534b11fc2f3e44654eb9
VincentSD <vincentsd@yahoo.com>
2019-10-28 18:23:02 +0100

    alexandre arrete de push putain

--- MODELS/models_message.php
+++ MODELS/models_message.php
@@ -195,7 +195,7 @@ class message extends base
 
     }
 
-
+//
 
     public function verifMsg () {
         if(preg_match("#^[ ]*[a-zA-Z0-9.-_]+[ ]*[a-zA-Z0-9.-_]+[ ]*$#",$_POST['msg'])) return true;
