36bc21b83fc1fb27b9ab179a032a14289ab8891a
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-28 18:17:23 +0100

    messages

--- MODELS/models_message.php
+++ MODELS/models_message.php
@@ -178,7 +178,7 @@ class message extends base
         }
     }
     public function verifMsg () {
-        if(preg_match("#^[ ]*[a-zA-Z0-9.-_]+[ ]*[a-zA-Z0-9]+[ ]*[a-zA-Z0-9.-_]+[ ]*$#",$_POST['msg'])) return true;
+        if(preg_match("#^[ ]*[a-zA-Z0-9.-_]+[ ]*[a-zA-Z0-9]+[ ]*[.-_]+[ ]*$#",$_POST['msg'])) return true;
 
         else {
             throw new Exception('Le message est trop grand ou comporte plus de 2 mots');
