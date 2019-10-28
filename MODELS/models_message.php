fb98393ecc7ae0d22dc8f1ecfbe5cbaf16ac2722
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-10-28 18:12:27 +0100

    messages

--- MODELS/models_message.php
+++ MODELS/models_message.php
@@ -179,7 +179,6 @@ class message extends base
     }
 
 
-
     public function verifMsg () {
         if(preg_match("#^[ ]*[a-zA-Z0-9.-_]+[ ]*[a-zA-Z0-9]+[ ]*[.-_]+[ ]*$#",$_POST['msg'])) return true;
 
