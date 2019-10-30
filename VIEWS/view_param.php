3e874a0384331dd78f15310c91cf1c3310f3f272
VincentSD <vincentsd@yahoo.com>
2019-10-30 21:33:22 +0100

    page admin

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -4,10 +4,13 @@
     <section class="offset-4 col-lg-5 text-center">
         <div class="row">
         <form  action="/ProjetPhp/admin"  method="post">
+            <label> chosir le nombre maximum de discussions ouvertes</label>
             <input type="text" placeholder="Nombre  de discussions" name="d1"/>
             <button type="submit" name="action" value="changeNbDisc"> Valider </button> <br> <br>
+            <label> nombre maximum de message dans une discussion </label>
             <input type="text" placeholder="Nombre de messages" name="d2"/>
             <button type="submit" name="action" value="changeNbMsg"> Valider </button> <br> <br>
+            <label> supprimer un utilisateur </label>
             <input type="text" placeholder="Saisir le Pseudo" name="d3"/>
             <button type="submit" name="action" value="delUser"> Supprimer </button> <br> <br>
 <!--            <input type="text" placeholder="Saisir le Titre" name="d4"/>-->
