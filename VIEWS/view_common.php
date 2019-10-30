bb345f48298e062728bd0214cdf0721b2d02be7b
VincentSD <vincentsd@yahoo.com>
2019-10-30 15:46:15 +0100

    test logo admin

--- VIEWS/view_common.php
+++ VIEWS/view_common.php
@@ -30,7 +30,7 @@ $user=unserialize($_SESSION['user']);
 
     <header class="row">
         <?php
-        if($user->getState() == 'admin')
+        if( isset($user) && $user->getState() == 'admin')
         {
         ?>
          <div class="col-xs-2 col-lg-2 text-center"> <a href="http://tpphp.alwaysdata.net/ProjetPhp">
