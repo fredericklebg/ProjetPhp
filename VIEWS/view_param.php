9ae2c1f6eca1a25f3f62724274b930f822f06b99
VincentSD <vincentsd@yahoo.com>
2019-10-30 21:25:25 +0100

    page admin

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -4,14 +4,14 @@
     <section class="offset-4 col-lg-4 text-center">
         <div class="row">
         <form  action="/ProjetPhp/admin"  method="post">
-            <input type="text" placeholder="Nombre  de discussions" name="d1"/> <br>
-            <input type="submit" name="action" value="Valider"/>
-            <input type="text" placeholder="Nombre de messages" name="d2"/> <br>
-            <input type="submit" name="action" value="Valider"/>
-            <input type="text" placeholder="Saisir le Pseudo" name="d3"/> <br>
-            <input type="submit" name="action" value="Supprimer"/>
-            <input type="text" placeholder="Saisir le Titre" name="d4"/> <br>
-            <input type="submit" name="action" value="Supprimer"/>
+            <input type="text" placeholder="Nombre  de discussions" name="d1"/>
+            <input type="submit" name="action" value="Valider"/> <br>
+            <input type="text" placeholder="Nombre de messages" name="d2"/>
+            <input type="submit" name="action" value="Valider"/> <br>
+            <input type="text" placeholder="Saisir le Pseudo" name="d3"/>
+            <input type="submit" name="action" value="Supprimer"/> <br>
+            <input type="text" placeholder="Saisir le Titre" name="d4"/>
+            <input type="submit" name="action" value="Supprimer"/> <br>
 
 <!--            <button type="submit" value="Validerrrr"> Valider </button>-->
         </form>
