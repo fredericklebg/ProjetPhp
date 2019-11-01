08bc9480cfa10952242cfacbda815962beab78c3
VincentSD <vincentsd@yahoo.com>
2019-11-01 16:35:51 +0100

    discussions

--- VIEWS/view_discussion.php
+++ VIEWS/view_discussion.php
@@ -59,7 +59,7 @@
                                             message
                                         </button>
                                     <? }
-                                    }
+                                    }//
                                     else
                                     {
                                      ?>
