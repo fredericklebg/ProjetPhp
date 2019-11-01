e6f10c31164bfb501b6bcb89d070a3e8a8f1db02
Boulahrouz Ibrahim <ibrahim.boulahrouz@etu.univ-amu.fr>
2019-11-01 13:36:04 +0100

    test all user

--- VIEWS/view_alluser.php
+++ VIEWS/view_alluser.php
@@ -1,3 +1,18 @@
+<?php $this->title ='FreeNote'; ?>
+
+<?php
+
+$disc= new discussion();
+?>
+
+<tr>
+                        <td> Nom de la discussion </td>
+                        <td>Statut</td>
+                        <td>Dernier message</td>
+                        <td>Auteur</td>
+                        <td>Date</td>
+                    </tr>
+
 <?php
 
                         $id = $row['user_id'];
@@ -8,3 +23,16 @@
 
                         </tr>
 
+                        <div class="col-lg-12 text-center">
+                <? if($page > 1) { ?>
+                <a href="/ProjetPhp/accueil//<?php echo $page-1; ?>"  ><input type="image" alt="previous" src="VIEWS/Media/gauche.jpg" width="30px" height="30px"> </a>
+                <?php
+                                 }
+                if ($page < $nbPages) { ?>
+                       <a href="/ProjetPhp/accueil//<? echo $page + 1 ?> "><input type="image" alt="next" src="VIEWS/Media/droite.jpg" width="30px" height="30px"> </a>
+                <?php
+                }
+                ?>
+            </div>
+
+        </section>
