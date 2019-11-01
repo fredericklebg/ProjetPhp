b71bb92abd8159c8c59ac3a7eed3eaddae69900e
VincentSD <vincentsd@yahoo.com>
2019-11-01 18:56:12 +0100

    delete disc

--- MODELS/models_discussion.php
+++ MODELS/models_discussion.php
@@ -106,11 +106,14 @@ class discussion extends base
        $query = $this->loadDb()->prepare($query);
        $query->bindValue('id',$id,PDO::PARAM_INT);
        $query->execute();
+       var_dump($query);
 
        $query2 = 'DELETE FROM DISCUSSION WHERE disc_id=:id';
        $query2 = $this->loadDb()->prepare($query2);
        $query2->bindValue('id',$id,PDO::PARAM_INT);
        $query2->execute();
+       var_dump($query2);
+       exit();
      }
 //
 
