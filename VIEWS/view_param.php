5bda6cbc69b019a50bf85ae52131c1b290dbc97d
VincentSD <vincentsd@yahoo.com>
2019-11-01 13:56:32 +0100

    data et admin

--- VIEWS/view_param.php
+++ VIEWS/view_param.php
@@ -10,6 +10,9 @@
             <label> Nombre maximum de messages dans une discussion </label>
             <input type="text" placeholder="Nombre de messages" name="d2"/>
             <button type="submit" name="action" value="changeNbMsg"> Valider </button> <br> <br>
+            <label> Pagination </label>
+            <input type="text" placeholder="nombre de discussions par page" name="d2"/>
+            <button type="submit" name="action" value="changePagination"> Valider </button> <br> <br>
             <label> Supprimer un utilisateur </label>
             <input type="text" placeholder="Saisir le Pseudo" name="d3"/>
             <button type="submit" name="action" value="delUser"> Supprimer </button> <br> <br>
