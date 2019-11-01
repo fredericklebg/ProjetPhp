e777c6be742bff25369904b07502aaacbaef1aae
VincentSD <vincentsd@yahoo.com>
2019-11-01 19:00:53 +0100

    graspoutine

--- MODELS/models_discussion.php
+++ MODELS/models_discussion.php
@@ -102,18 +102,19 @@ class discussion extends base
 
      public function delDisc($id)
      {
-       $query = 'DELETE FROM MESSAGE WHERE disc_id=:id';
-       $query = $this->loadDb()->prepare($query);
-       $query->bindValue('id',$id,PDO::PARAM_INT);
-       $query->execute();
-       var_dump($query->execute());
-
-       $query2 = 'DELETE FROM DISCUSSION WHERE disc_id=:id';
-       $query2 = $this->loadDb()->prepare($query2);
-       $query2->bindValue('id',$id,PDO::PARAM_INT);
-       $query2->execute();
-       var_dump($query2->execute());
-       exit();
+//       $query = 'DELETE FROM MESSAGE WHERE disc_id=:id';
+//       $query = $this->loadDb()->prepare($query);
+//       $query->bindValue('id',$id,PDO::PARAM_INT);
+//       $query->execute();
+         $query='DELETE FROM MESSAGE WHERE disc_id= \'' . $id . '\' ';
+         $this->execRequete($query);
+//
+//       $query2 = 'DELETE FROM DISCUSSION WHERE disc_id=:id';
+//       $query2 = $this->loadDb()->prepare($query2);
+//       $query2->bindValue('id',$id,PDO::PARAM_INT);
+//       $query2->execute();
+         $query2='DELETE FROM DISCUSSION WHERE disc_id= \'' . $id . '\' ';
+         $this->execRequete($query2);
      }
 
 
