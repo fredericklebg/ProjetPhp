5d8da945e0f496afc6d7b7b1f5d5f0b259ca7770
Alexandre SALLES <alexandre.salles@etu.univ-amu.fr>
2019-11-02 14:07:51 +0100

    graspoutine

--- CONTROLLERS/controller_admin.php
+++ CONTROLLERS/controller_admin.php
@@ -40,7 +40,7 @@ class controller_admin extends controller_main
     public function Supprimer()
     {
         $userSupp=$_POST['aurevoir'];
-        if ($this->user->isAdmin($userSupp)==null) {
+        if ($this->user->isAdmin($userSupp)) {
             throw new Exception('Admin impossible à supprimer');
         }
 
