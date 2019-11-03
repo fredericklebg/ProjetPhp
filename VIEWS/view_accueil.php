b31138f00dbb7e1ebfcabcd7e44acdeae924435d
VincentSD <vincentsd@yahoo.com>
2019-11-03 14:53:01 +0100

    w3c

--- VIEWS/view_accueil.php
+++ VIEWS/view_accueil.php
@@ -69,11 +69,11 @@ $disc= new discussion();
                 </table>
             <div class="col-lg-12 text-center">
                 <? if($page > 1) { ?>
-                <a href="/ProjetPhp/accueil//<?php echo $page-1; ?>"  ><input type="image" alt="previous" src="VIEWS/Media/gauche.jpg" width="30px" height="30px"> </a>
+                <a href="/ProjetPhp/accueil//<?php echo $page-1; ?>"  ><input type="image" alt="previous" src="VIEWS/Media/gauche.jpg"  height="30"> </a>
                 <?php
                                  }
                 if ($page < $nbPages) { ?>
-                       <a href="/ProjetPhp/accueil//<? echo $page + 1 ?> "><input type="image" alt="next" src="VIEWS/Media/droite.jpg" width="30px" height="30px"> </a>
+                       <a href="/ProjetPhp/accueil//<? echo $page + 1 ?> "><input type="image" alt="next" src="VIEWS/Media/droite.jpg"  height="30"> </a>
                 <?php
                 }
                 ?>
