9a0eb53a09fed6a43978a54430ab49035b2db6ba
VincentSD <vincentsd@yahoo.com>
2019-11-03 15:56:16 +0100

    responsive admin

--- VIEWS/view_admin.php
+++ VIEWS/view_admin.php
@@ -7,9 +7,9 @@ if( $user != null && $user->getState() == 'admin')
 ?>
 <div class="container">
 
-    <div class="offset-4 col-lg-5 text-center">
+<!--    <div class="offset-4 col-lg-5 text-center">-->
         <div class="row">
-        <form  action="/ProjetPhp/admin"  method="post">
+        <form class="col-lg-12 text-center" action="/ProjetPhp/admin"  method="post">
             <label> Choisir le nombre maximum de discussions ouvertes</label>
             <input type="text" placeholder="Nombre de discussions" name="d1"/>
             <button type="submit" name="action" value="changeNbDisc"> Valider </button> <br> <br>
@@ -33,7 +33,7 @@ if( $user != null && $user->getState() == 'admin')
 
 
     </div>
-</div>
+<!--</div>-->
 
 <?php
     }
